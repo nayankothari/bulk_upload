31a4d8e83163d22cdc1beb98a1bd316e7dfb8424
Mr. Guest <nnkothari86@gmail.com>
2025-05-18 20:16:12 +0530

    Added debug tool bar to check for over uses of queries

--- conf/settings.py
+++ conf/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'rest_framework',
     'api'
 ]
@@ -37,7 +38,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 ROOT_URLCONF = 'conf.urls'
 
